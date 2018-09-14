e0ef1cfd7119a6e557fd3138d3a0338aa3dcc6af
Ethan K <3than.katz@gmail.com>
2018-09-14 18:54:41 -0400

    HW 1

---
+++ Scripts/script.js
@@ -0,0 +1,15 @@
+/*
+ORIGINAL PROGRAM:
+-----------------
+const div = document.querySelector("div");
+div.innerHTML = "<p>Over the wintry</p>";
+div.innerHTML += "<p>forest, winds howl in rage</p>";
+div.innerHTML += "<p>with no leaves to blow.</p>";
+
+*/
+
+const div = $('div');
+
+div.append('<p>"Over the wintry"</p>');
+div.append('<p>"forest, winds howl in rage"</p>');
+div.append('<p>"with no leaves to blow."</p>');
