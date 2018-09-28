b618e3886a62369413111e1a3e8485e8cb2f106d
Ethan Katz <3than.katz@gmail.com>
2018-09-28 17:10:40 -0400

    Test

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,15 +1,1 @@
-/*
-ORIGINAL PROGRAM:
------------------
-const div = document.querySelector("div");
-div.innerHTML = "<p>Over the wintry</p>";
-div.innerHTML += "<p>forest, winds howl in rage</p>";
-div.innerHTML += "<p>with no leaves to blow.</p>";
-
-*/
-
-const div = $('div');
-
-div.append('<p>"Over the wintry"</p>');
-div.append('<p>"forest, winds howl in rage"</p>');
-div.append('<p>"with no leaves to blow."</p>');
+let input = alert("Hey there!!");
